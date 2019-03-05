eaa808ed75b5daa5f1d02c138e31fe43652ebd9a
tranphuquy19 <tranphuquy19@gmail.com>
2019-03-05 23:55:42 +0700

    New chapter: 3.1 Sample app setup

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_many :microposts
-    validates :name, presence: true    # Replace FILL_IN with the right code.
-    validates :email, presence: true    # Replace FILL_IN with the right code.
-  end
-  
